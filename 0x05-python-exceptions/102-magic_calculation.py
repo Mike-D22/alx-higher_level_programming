9e613060c536240d2f78b98788d066905d184e42
Mike-D22 <qamp26@gmail.com>
2023-08-28 16:32:15 -0700

    A Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode.

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    for n in range(1, 3):
+        try:
+            if n > a:
+                raise ValueError('Too large')
+            else:
+                result += a ** b / n
+        except ValueError as ve:
+            print(f"Caught a ValueError: {ve}")
+            result = b + a
+            break
+    return result
